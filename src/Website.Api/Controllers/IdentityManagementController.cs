0c1810d872a1086b9f6ffead87cb132ea44b2415
ArcticGizmo <jon.howell314@gmail.com>
2023-07-23 15:49:23 +0800

    start of user management controller

--- src/Website.Api/Controllers/IdentityManagementController.cs
+++ src/Website.Api/Controllers/IdentityManagementController.cs
@@ -9,12 +9,49 @@ namespace Website.Api.Controllers;
 
 [ApiController]
 [Route("[controller]")]
-[Authorize]
+[Authorize(WbesiteScopes.ManageIdentity)]
+[Authorize(FgaAuthorizationDefaults.PolicyKey)]
+[FgaRole(FgaRoles.Admin)]
 public class IdentityManagementController : ControllerBase
 {
     private readonly OpenFgaClient _fga;
     public IdentityManagementController(OpenFgaClient fga)
     {
         _fga = fga;
+    }
+
+    [HttpGet("users")]
+    public async Task GetUsers()
+    {
+        Console.WriteLine("get all users");
+    }
+
+    [HttpGet("users/{id}")]
+    public async Task GetUser(string id)
+    {
+        // how do we manage user permissions?
+        Console.WriteLine("Getting specific user");
+    }
+
+    // TODO: should this be patch instead?
+    [HttpPost("users/{:id}/roles")]
+    public async Task UpdateRoles()
+    {
+        Console.WriteLine("Updating user roles");
+        // not sure what happens when creating more than 10 tuples at a time
+    }
+
+    [HttpPost("invite-user")]
+    public async Task InviteUser()
+    {
+        // I think we will need to switch to organization flows to restrict users logging in
+        Console.WriteLine("Invite User");
+    }
+
+    [HttpPost("remove-user")]
+    public async Task RemoveUser()
+    {
+        Console.WriteLine("Remove User");
+        // this will be complicated as we also need to remove all permissions within openFga
     }
 }
